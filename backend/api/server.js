e7e188b03e4f562ea5bf37c03b68449569c7a05b
khizer12khohar <211400037@gift.edu.pk>
2026-03-12 13:31:42 +0500

    2

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("../routes/userRoutes")
 
 const app = express()
 
